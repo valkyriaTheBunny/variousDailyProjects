949cff3cea6769b8f2f78bc944d249dae8d5da31
beandrea <arkflame99@gmail.com>
2025-03-31 14:01:25 -0400

    fully working methodology for a three stack system within one list

--- threeStacks.py
+++ threeStacks.py
@@ -1,36 +1,77 @@
 class Stack:
     def __init__(self):
         self.__stack = []
-        self.__lastIndex = 0
 
     def push(self, item: any, stack_num: int) -> None:
-        if stack_num > 3 and stack_num < 1:
+        if stack_num > 3 or stack_num < 1:
             return
 
-        if self.__lastIndex > stack_num:
-            index = stack_num
-            while self.__stack[index] != None and index < len(self.__stack):
-                index += stack_num
-            if index < len(self.__stack):
-                self.__stack.insert(index, item)
-        elif self.__lastIndex % stack_num != 0:
-            while self.__lastIndex % stack_num != 0:
-                self.__stack.append(None)
-                self.__lastIndex += 1
-            self.__stack.append(item)
-            self.__lastIndex += 1
-        else:
-            self.__stack.append(item)
-            self.__lastIndex += 1
+        index = stack_num - 1
+
+        while index < len(self.__stack) and self.__stack[index] is not None:
+            index += 3
+
+        while len(self.__stack) <= index:
+            self.__stack.append(None)
+
+        self.__stack[index] = item
+
 
     def pop(self, stack_num: int) -> any:
-        if self.__lastIndex % stack_num == 0:
-            return self.__stack.pop()
-        else:
-            index = self.__lastIndex
-            while index % stack_num != 0:
-                index -= 1
-            return self.__stack.pop(index)
+        if stack_num > 3 or stack_num < 1:
+            return None
+
+        index =  stack_num - 1
+        lastValid = -1
+
+        while index < len(self.__stack):
+            if self.__stack[index] is not None:
+                lastValid = index
+            index += 3
+
+        if lastValid != -1:
+            item = self.__stack[lastValid]
+            self.__stack[lastValid] = None
+            return item
+        
+        return None
+
+    def __eq__(self, obj: any) -> bool:
+        return self.__stack == obj
 
 def test_stack_creation():
-    pass
+    tStack = Stack()
+    assert tStack == []
+
+def test_stack_push_first_stack():
+    tStack2 = Stack()
+    tStack2.push(9, 1)
+    assert tStack2 == [9]
+
+def test_stack_push_second_stack():
+    tStack3 = Stack()
+    tStack3.push(9, 2)
+    assert tStack3 == [None, 9]
+
+def test_stack_push_third_stack():
+    tStack3 = Stack()
+    tStack3.push(9, 3)
+    assert tStack3 == [None, None, 9]
+
+def test_stack_push_stacks_with_existing():
+    tStack4 = Stack()
+    tStack4.push(9, 3)
+    tStack4.push(8, 2)
+    assert tStack4 == [None, 8, 9]
+
+def test_stack_push_stacks_with_existing_at_index():
+    tStack4 = Stack()
+    tStack4.push(9, 3)
+    tStack4.push(8, 3)
+    assert tStack4 == [None, None, 9, None, None, 8]
+
+def test_stack_pop_stacks_with_multiple_at_index():
+    tStack4 = Stack()
+    tStack4.push(9, 3)
+    tStack4.push(8, 3)
+    assert tStack4.pop(3) == 8
